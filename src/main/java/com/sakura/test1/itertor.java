219fbf065e801234e88a2ba2daed0898c81c5aab
diaosakura <diaosakura@gmail.com>
2026-03-16 22:13:41 +0800

    抢红包

---
+++ src/main/java/com/sakura/test1/itertor.java
@@ -0,0 +1,16 @@
+package com.sakura.test1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class itertor {
+    public static void main(String[] args) {
+        Iterable<String> iter;
+        List<String> list = new ArrayList<String>();
+        int[] arr = new int[]{1,2,3,4,2,6,4,2,1};
+        arr[2]=1;
+        int[] arr2 =new int[3];
+        arr2 [2] = 1;
+    }
+
+}
